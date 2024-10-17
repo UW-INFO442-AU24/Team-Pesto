b66079b6f839d0e5991c5b1559db269aff01a51b
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-10-17 15:03:28 -0700

    set up structure of backend

---
+++ frontend/components/mood_tracking.jsx
@@ -0,0 +1,14 @@
+import React , {useState } from 'react';
+import axios from 'axios'
+
+const mood_tracking = () => {
+  const [mood, setMood] = useState[null]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default mood_tracking

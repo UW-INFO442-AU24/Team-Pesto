a5c7884f2f8843acca4adfce6b8df4439aa13125
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-11-12 12:04:59 -0800

    bug fix: make sure that users are logged in and can only read/change their own moods

--- backend/api/routers/moods.py
+++ backend/api/routers/moods.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, Depends, HTTPException
-from typing import List
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 from api.crud import mood as crud_mood
 from api.schemas.mood import Mood, MoodCreate
 from db.db_setup import get_db
+from db.models.user import User
 from utils.utils import get_current_active_user
 
 router = APIRouter()
 
-@router.post("/users/{user_id}/moods/", response_model=Mood)
-def create_mood_for_user(user_id: int, mood: MoodCreate, db: Session = Depends(get_db)):
-    return crud_mood.create_user_mood(db=db, mood=mood, user_id=user_id)
+# make sure that only users that are logged in can change and read moods
+@router.post("/moods/", response_model=Mood)
+def create_mood_for_user(mood: MoodCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+    return crud_mood.create_user_mood(db=db, mood=mood, user_id=current_user.id)
 
-@router.get("/users/{user_id}/moods/", response_model=List[Mood])
-def read_moods_for_user(user_id: int, db: Session = Depends(get_db)):
-    return crud_mood.get_moods_by_user(db=db, user_id=user_id)
+@router.get("/moods/", response_model=List[Mood])
+def read_moods_for_user(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+    return crud_mood.get_moods_by_user(db=db, user_id=current_user.id)

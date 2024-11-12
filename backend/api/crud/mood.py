0ac46845fce99c6ac753db56b9d9906969681b01
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-11-12 13:00:09 -0800

    add ability to read single mood, add ability to update user profile

--- backend/api/crud/mood.py
+++ backend/api/crud/mood.py
@@ -9,5 +9,10 @@ def create_user_mood(db: Session, mood: MoodCreate, user_id: int):
     db.refresh(db_mood)
     return db_mood
 
-def get_moods_by_user(db: Session, user_id: int):
-    return db.query(Mood).filter(Mood.user_id == user_id).all()
+def get_mood_from_user(db: Session, mood_id: int, user_id: int):
+    mood = db.query(Mood).filter(Mood.id == mood_id, Mood.user_id == user_id).first()
+    return mood
+
+def get_all_moods_by_user(db: Session, user_id: int):
+    moods = db.query(Mood).filter(Mood.user_id == user_id).all()
+    return moods
--- backend/api/routers/moods.py
+++ backend/api/routers/moods.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from api.crud import mood as crud_mood
@@ -14,6 +14,13 @@ router = APIRouter()
 def create_mood_for_user(mood: MoodCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     return crud_mood.create_user_mood(db=db, mood=mood, user_id=current_user.id)
 
+@router.get("/moods/{mood_id}", response_model=Mood)
+def read_mood(mood_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+    db_mood = crud_mood.get_mood_from_user(db=db, mood_id=mood_id, user_id=current_user.id)
+    if db_mood is None:
+        raise HTTPException(status_code=404, detail="Mood not found")
+    return db_mood
+
 @router.get("/moods/", response_model=List[Mood])
-def read_moods_for_user(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
-    return crud_mood.get_moods_by_user(db=db, user_id=current_user.id)
+def read_all_moods_for_user(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+    return crud_mood.get_all_moods_by_user(db=db, user_id=current_user.id)

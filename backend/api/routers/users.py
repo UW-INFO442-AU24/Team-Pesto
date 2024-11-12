8d073015cde17e3cb589c7cbc790b9c2a160329d
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-11-12 13:00:09 -0800

    add ability to read single mood, add ability to update user profile

--- backend/api/routers/users.py
+++ backend/api/routers/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from api.crud import auth as crud_user
-from api.schemas.auth import User, UserCreate
+from api.crud import auth as crud_auth
+from api.crud import user as crud_user
+from api.schemas.auth import User, UserCreate, UserUpdate
 from db.db_setup import get_db
 from utils.utils import get_current_active_user
 
@@ -9,11 +10,15 @@ router = APIRouter()
 
 @router.post("/", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = crud_user.get_user(db, username=user.username)
+    db_user = crud_auth.get_user(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    return crud_user.create_user(db=db, user=user)
+    return crud_auth.create_user(db=db, user=user)
 
 @router.get("/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
+@router.put("/me/", response_model=User)
+async def update_user_me(user_update: UserUpdate, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+    return crud_user.update_user(db=db, user_update=user_update, user_id=current_user.id)

ba82643d151c8edb74119ca7d882d17f189a00ee
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2024-11-14 14:15:55 -0800

    Update self_assessments.py

--- backend/api/routers/self_assessments.py
+++ backend/api/routers/self_assessments.py
@@ -1,3 +1,12 @@
-import fastapi
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
 
-router = fastapi.APIRouter()
+from api.crud import self_assessment as crud_test
+from api.schemas.self_assessment import Question, Answer, Response
+from db.db_setup import get_db
+from db.models.user import User
+from utils.utils import get_current_active_user
+
+router = APIRouter()
+

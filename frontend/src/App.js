80652764015eb7834cecb6f2b4577116d5ed5300
mynahshetty11 <shettymynah22@gmail.com>
2024-11-26 15:22:52 -0800

    commiting link to self-assessment in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Login from './components/login_form'
 import Signup from './components/signup_form'
-import Homepaage from './components/homepage'
-
+import Homepage from './components/homepage'
+import SelfAssessmentQuiz from './components/self-assessment-quiz'
 const App = () => {
   return (
     <div>

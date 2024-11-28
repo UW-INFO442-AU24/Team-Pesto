5071aa188d954866cce47bd2daef0195e3d26d35
Katie Shi <katieshi@uw.edu>
2024-11-27 16:36:54 -0800

    adding resources page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "../src/components/header.js";
 import Homepage from "../src/components/homepage.js";
 import AboutUs from "../src/components/about-us.js"
 import SelfAssessmentQuiz from './components/self-assessment-quiz'
+import Resources from './components/resources.js'
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-        <Route path="/homepage" exact>
-          <h1>Home Page</h1>
-        </Route>
-        <Route path="/about-us" exact>
-          <h1>About Us</h1>
-        </Route>
-        <Route path="/wellness-history">
-          <h1>Wellness History</h1>
-        </Route>
-        <Route path="/resources">
-          <h1>Resources</h1>
-        </Route>
-        <Route path="/profile">
-          <h1>Profile</h1>
-        </Route>
+    <div>
+      
+      <Router>
+        {/* <Header /> */}
+          {/* <Route path="/homepage" element={<Homepage />} />
+          <Route path="/about-us" exact>
+            <h1>About Us</h1>
+          </Route>
+          <Route path="/wellness-history">
+            <h1>Wellness History</h1>
+          </Route> */}  
+          <Routes>
+            <Route path="/" element={<Resources />} />
+          </Routes>
+          {/* <Route path="/profile">
+            <h1>Profile</h1>
+          </Route> */}
 
-    </Router>
+      </Router>
+    </div>
   );
 }
 
